e9cec0373c5b12a014e1f92bc2128c0e01c8e20f
sravan <banavaramsravankumar@gmail.com>
2022-12-25 21:30:46 +0530

    Coomit1 for today

---
+++ AJautomation/src/test/java/com/aj/automation/Sample1.java
@@ -0,0 +1,22 @@
+package com.aj.automation;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+	@Test
+	public void a()
+	{
+		System.out.println("a");
+	}
+	@Test
+	public void b()
+	{
+		System.out.println("b");
+	}
+	@Test
+	public void c()
+	{
+		System.out.println("c");
+	}
+
+}
---
+++ AJautomation/src/test/java/com/aj/automation/Sample2.java
@@ -0,0 +1,22 @@
+package com.aj.automation;
+
+import org.testng.annotations.Test;
+
+public class Sample2 {
+	@Test
+	public void d()
+	{
+		System.out.println("d");
+	}
+	@Test
+	public void e()
+	{
+		System.out.println("e");
+	}
+	@Test
+	public void f()
+	{
+		System.out.println("f");
+	}
+
+}
